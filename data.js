326bb8d5542b36d88e67b1319df725f6ae1694c4
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-01 17:36:25 +0530

    props object

---
+++ data.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/14686152/pexels-photo-14686152.jpeg",
+    name: "Waffle with Berries",
+    type: "Waffle",
+    price: 6.5
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/8753645/pexels-photo-8753645.jpeg",
+    name: "Vanilla Bean Crème Brûlée",
+    type: "Crème Brûlée",
+    price: 7.0
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/586830/pexels-photo-586830.jpeg",
+    name: "Macaron Mix of Five",
+    type: "Macaron",
+    price: 8.0
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/1247672/pexels-photo-1247672.jpeg",
+    name: "Classic Tiramisu",
+    type: "Tiramisu",
+    price: 5.5
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/36169157/pexels-photo-36169157.jpeg",
+    name: "Pistachio Baklava",
+    type: "Baklava",
+    price: 4.0
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/30391247/pexels-photo-30391247.jpeg",
+    name: "Lemon Meringue Pie",
+    type: "Pie",
+    price: 5.0
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/19534496/pexels-photo-19534496.jpeg",
+    name: "Red Velvet Cake",
+    type: "Cake",
+    price: 4.5
+  },
+  {
+    id: 8,
+    img: "https://images.pexels.com/photos/7716556/pexels-photo-7716556.jpeg",
+    name: "Salted Caramel Brownie",
+    type: "Brownie",
+    price: 4.5
+  },
+  {
+    id: 9,
+    img: "https://images.pexels.com/photos/6911838/pexels-photo-6911838.jpeg",
+    name: "Vanilla Panna Cotta",
+    type: "Panna Cotta",
+    price: 6.5
+  }
+];
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,16 +1,15 @@
 function Dish(props) {
   return (
     <div className="card-container">
-   
-        <img src={props.img} alt={props.name} className="dessert-img" />
-        <button className="add-btn"><span class="material-symbols-outlined">
-shopping_cart
-</span> Add to Cart</button>
-     
+      <img src={props.dish.img} alt={props.dish.name} className="dessert-img" />
+      <button className="add-btn">
+        <span class="material-symbols-outlined">shopping_cart</span> Add to Cart
+      </button>
+
       <div className="details">
-        <p className="dessert-type">{props.type}</p>
-        <h4 className="dessert-name">{props.name}</h4>
-        <p className="dessert-cost">{props.cost}</p>
+        <p className="dessert-type">{props.dish.type}</p>
+        <h4 className="dessert-name">{props.dish.name}</h4>
+        <p className="dessert-cost">{props.dish.cost}</p>
       </div>
     </div>
   );
